8863f2445f9d74b504245ad417c01f1d3ab01414
Sonad <sonad395@gmail.com>
2017-03-08 13:41:53 +0300

    SimOperation добавлен констурктор и геттеры/сеттеры

--- entities/SimOperation.java
+++ entities/SimOperation.java
@@ -8,4 +8,44 @@ public class SimOperation {
     private String simNumber;
     private String dateOfIssue;
     private String dateOfDeath;
+
+    public SimOperation(String passportNumber, String simNumber, String dateOfIssue, String dateOfDeath) {
+        this.passportNumber = passportNumber;
+        this.simNumber = simNumber;
+        this.dateOfIssue = dateOfIssue;
+        this.dateOfDeath = dateOfDeath;
+    }
+
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    public void setPassportNumber(String passportNumber) {
+        this.passportNumber = passportNumber;
+    }
+
+    public String getSimNumber() {
+        return simNumber;
+    }
+
+    public void setSimNumber(String simNumber) {
+        this.simNumber = simNumber;
+    }
+
+    public String getDateOfIssue() {
+        return dateOfIssue;
+    }
+
+    public void setDateOfIssue(String dateOfIssue) {
+        this.dateOfIssue = dateOfIssue;
+    }
+
+    public String getDateOfDeath() {
+        return dateOfDeath;
+    }
+
+    public void setDateOfDeath(String dateOfDeath) {
+        this.dateOfDeath = dateOfDeath;
+    }
 }
+
