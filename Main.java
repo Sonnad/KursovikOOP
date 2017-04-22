b2530ef6ed4aa857e6de73c10b770ec8d4d539c4
Sonad <sonad395@gmail.com>
2017-04-22 11:44:35 +0300

    Разрабатывается меню пользователя

--- Main.java
+++ Main.java
@@ -6,29 +6,61 @@ import entities.Client;
 import entities.Sim;
 import structures.*;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Hash simBase = new Hash();
-//        for (int i = 0; i < 300 ; i++) {
-//            simBase.addElement(new Sim(Hash.keyGeneration(), "Тариф" + i, i, true));
-//        }
-        int count = 0;
-        simBase.addElement(new Sim("123456789", "black", 2014, true));
-        simBase.addElement(new Sim("123456389", "black", 2014, true));
-        simBase.addElement(new Sim("133456789", "black", 2014, true));
-        simBase.addElement(new Sim("153456389", "black", 2014, true));
-        simBase.addElement(new Sim("123456729", "black", 2014, true));
-        simBase.removeElement("153456389");
-        for (int i = 0; i < simBase.getHashTableLength(); i++) {
-            System.out.println(" [" + i + "]  " + simBase.getHashTable()[i]);
-            if (simBase.getHashTable()[i] != null) count++;
+
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\tМЕНЮ");
+        while (true)
+        {
+            System.out.println("\t\tНажмите соответствующую клавишу на клавиатуре для выбора нужного пункта меню.\n");
+            System.out.println(
+                    "1 - Работа с симкартами \n" +
+                    "2 - Работа с клиентами\n" +
+                    "3 - Выход\n"
+            );
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+                System.out.print("Ваш выбор: ");
+                int choice = Integer.parseInt(reader.readLine());
+                switch (choice)
+                {
+                    case 1: {
+                        System.out.println(
+                                        "1 - Добавить сим карту \n" +
+                                        "2 - Работа с клиентами\n" +
+                                        "3 - Вернуться в предыдущее меню\n"
+                        );
+                        System.out.print("Ваш выбор: ");
+                        choice = Integer.parseInt(reader.readLine());
+                        switch (choice)
+                        {
+                            case 1: {
+                                boolean exit = false;
+                                while (!exit)
+                                {
+                                    System.out.println("Номер сим карты в формате NNN-NNNNNNN");
+                                }
+                            }
+                        }
+                        break;
+                    }
+                    case 2:
+                        System.in.read();
+                        break;
+                    case 3:
+                        return;
+                }
+
+            } catch(Exception e){};
         }
-        System.out.println(count);
     }
 
 }
