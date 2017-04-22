0cde6dabd901ba2770536b3d1ec4dda891d7c7ee
Sonad <sonad395@gmail.com>
2017-04-22 13:01:45 +0300

    SRC добавлено в Git

---
+++ operations/Operations.java
@@ -0,0 +1,14 @@
+package operations;
+
+import structures.Hash;
+
+/**
+ * Created by Sonad on 22.04.17.
+ */
+public class Operations {
+    static void simAdd(final Hash simBase)
+    {
+
+    }
+}
+
